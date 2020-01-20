09fd8281a7f0b57faf55a8203853bd3f4946d9ae
samuel.souza@dextra-sw.com <samuel.souza@dextra-sw.com>
2020-01-20 19:27:15 -0300

    Adding salvaLocal modifications

--- scripts/protoaculous.js
+++ scripts/protoaculous.js
@@ -6,7 +6,6 @@
  *
  *--------------------------------------------------------------------------*/
 
-var nomeSoft = 'ondas';
 var Prototype = {
   Version: '1.6.1',
 
@@ -7338,40 +7337,3 @@ function flashOnInit(applet)
 
 
 
-var SalvaLocal = new (Class.create({
-  Salva: function(soft, key, value) {
-    var item = localStorage.getItem(soft);
-    software = item ? JSON.parse(item) : {};
-    software[key] = value;
-    localStorage.setItem(soft, JSON.stringify(software));  
-  },
-  Pega: function(soft, key) {
-    item = localStorage.getItem(soft);
-    software = item ? JSON.parse(item) : {};
-    return software[key] ? software[key] : null;
-  },
-  Apaga: function (soft, key){
-    var item = localStorage.getItem(soft);
-
-    if(!item)
-      return
-
-    software = item ? JSON.parse(item) : {};
-    delete software[key];
-    localStorage.setItem(soft, JSON.stringify(software));    
-  },
-  ApagaTudo: function (soft) {
-    localStorage.removeItem(soft)
-  }
-}))
-
-var hades = $;
-
-$ = function(...args) {
-  if (args[0] === 'SalvaLocal' || args[0] === 'Mapinha') {
-    console.log("CAIU AQUI")
-    return SalvaLocal;
-  } else {
-    return hades(...args)
-  }
-};
